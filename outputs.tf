938f2d206d85c3e7d79ec79dde62fde01b209b28
Jan Buijnsters <jbui@netcompany.cloud>
2025-11-12 16:49:06 +0100

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "resource_id" {
+output "id" {
   description = "The ID of the resoure"
   value       = local.mysql_server.id
 }
 
-output "resource_name" {
+output "name" {
   description = "The name of the resource"
   value       = local.mysql_server.name
+}
+
+output "fqdn" {
+  description = "The name of the resource"
+  value       = local.mysql_server.fqdn
 }
